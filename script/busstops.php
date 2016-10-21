ae982166e00dacb5b05005494715e905e769b2ee
shrutikamtekar <shrutikamtekar@users.noreply.github.com>
2016-10-20 21:24:53 -0400

    updated removing personal info

--- script/busstops.php
+++ script/busstops.php
@@ -1,12 +1,12 @@
 <?php
   // PHP variable to store the host address
- $db_host  = "mysql6.000webhost.com";
+ $db_host  = "";
  // PHP variable to store the username
- $db_uid  = "a6054419_sb";
+ $db_uid  = "";
  // PHP variable to store the password
- $db_pass = "school1234";
+ $db_pass = "";
  // PHP variable to store the Database name  
- $db_name  = "a6054419_sb"; 
+ $db_name  = ""; 
         // PHP variable to store the result of the PHP function 'mysql_connect()' which establishes the PHP & MySQL connection  
  $db_con = mysql_connect($db_host,$db_uid,$db_pass) or die('could not connect');
  mysql_select_db($db_name);
