42f8853af9ee1572f370510c566b87c374b8ede5
shrutikamtekar <shrutikamtekar@users.noreply.github.com>
2016-10-20 21:29:20 -0400

    updated removing personal info

--- script/sms.php
+++ script/sms.php
@@ -1,12 +1,12 @@
 <?php
   // PHP variable to store the host address
- $db_host  = "127.0.0.1";
+ $db_host  = "";
  // PHP variable to store the username
- $db_uid  = "root";
+ $db_uid  = "";
  // PHP variable to store the password
  $db_pass = "";
  // PHP variable to store the Database name  
- $db_name  = "schoolbus"; 
+ $db_name  = ""; 
         // PHP variable to store the result of the PHP function 'mysql_connect()' which establishes the PHP & MySQL connection  
  $db_con = mysql_connect($db_host,$db_uid,"") or die('could not connect');
  mysql_select_db($db_name);
