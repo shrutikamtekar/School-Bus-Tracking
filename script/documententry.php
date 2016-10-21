8115b985705c332f7754dffea4c7b006b8ee44b2
shrutikamtekar <shrutikamtekar@users.noreply.github.com>
2016-10-20 21:26:05 -0400

    updated removing personal info

--- script/documententry.php
+++ script/documententry.php
@@ -2,13 +2,13 @@
 
 
   // PHP variable to store the host address
- $db_host  = "127.0.0.1";
+ $db_host  = "";
  // PHP variable to store the username
  $db_uid  = "root";
  // PHP variable to store the password
  $db_pass = "";
  // PHP variable to store the Database name  
- $db_name  = "schooladmin"; 
+ $db_name  = ""; 
         // PHP variable to store the result of the PHP function 'mysql_connect()' which establishes the PHP & MySQL connection  
  $db_con = mysql_connect($db_host,$db_uid,"") or die('could not connect');
  mysql_select_db($db_name);
